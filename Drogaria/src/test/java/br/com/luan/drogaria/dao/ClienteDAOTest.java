57a7fa73090924287f6ce11b483fdcea1c551460
TonhaodoArrroz <Luan.gustavo.melo@gmail.com>
2022-04-29 11:20:43 -0300

    DAO final e JSF e PrimeFaces

--- Drogaria/src/test/java/br/com/luan/drogaria/dao/ClienteDAOTest.java
+++ Drogaria/src/test/java/br/com/luan/drogaria/dao/ClienteDAOTest.java
@@ -14,102 +14,101 @@ public class ClienteDAOTest {
 	@Test
 	@Ignore
 	public void salvar() {
-		
+
 		long codigo = 3L;
 		PessoaDAO pessoaDAO = new PessoaDAO();
 		Pessoa pessoa = pessoaDAO.buscar(codigo);
-        
+
 		Cliente cliente = new Cliente();
 		cliente.setDataCadastro(new Date());
 		cliente.setLiberado(true);
-        cliente.setPessoa(pessoa);
-       
-        ClienteDAO clienteDAO = new ClienteDAO();
-        clienteDAO.salvar(cliente);
-        
-        System.out.println("Cliente salvo com sucesso : " + pessoa.getNome());
-		}
+		cliente.setPessoa(pessoa);
+
+		ClienteDAO clienteDAO = new ClienteDAO();
+		clienteDAO.salvar(cliente);
+
+		System.out.println("Cliente salvo com sucesso : " + pessoa.getNome());
+	}
+
 	// -------------- LISTAR ----------------------
-		@Ignore
-		@Test
-		public void listar() {
-			ClienteDAO clienteDAO = new ClienteDAO();
-			List<Cliente> resultado = clienteDAO.listar();
-			
-			for (Cliente cliente : resultado) {
-				System.out.println("Código do Cliente : " + cliente.getCodigo());
-				System.out.println("Nome do Cliente : " + cliente.getPessoa().getNome());
-				System.out.println("Cliente desde  : " + cliente.getDataCadastro());
-				
-				if(cliente.getLiberado() == true) {
-					System.out.println("Funcionário");
-				}else {
-					System.out.println("Cliente");
-				}
+	@Ignore
+	@Test
+	public void listar() {
+		ClienteDAO clienteDAO = new ClienteDAO();
+		List<Cliente> resultado = clienteDAO.listar();
+
+		for (Cliente cliente : resultado) {
+			System.out.println("Código do Cliente : " + cliente.getCodigo());
+			System.out.println("Nome do Cliente : " + cliente.getPessoa().getNome());
+			System.out.println("Cliente desde  : " + cliente.getDataCadastro());
+
+			if (cliente.getLiberado() == true) {
+				System.out.println("Funcionário");
+			} else {
+				System.out.println("Cliente");
 			}
-				
-				System.out.println();
-			}
-		// -------------- BUSCAR ----------------------
-		@Test
-		@Ignore
-		public void buscar() {
-			
-			long codigo = 1L;
-			ClienteDAO clienteDAO = new ClienteDAO();
-			Cliente cliente = clienteDAO.buscar(codigo);
-			
-			if(cliente == null) {
-				System.out.println("Nenhum registro encontrado");
-			}else {
+		}
+
+		System.out.println();
+	}
+
+	// -------------- BUSCAR ----------------------
+	@Test
+	@Ignore
+	public void buscar() {
+
+		long codigo = 1L;
+		ClienteDAO clienteDAO = new ClienteDAO();
+		Cliente cliente = clienteDAO.buscar(codigo);
+
+		if (cliente == null) {
+			System.out.println("Nenhum registro encontrado");
+		} else {
 			System.out.println("Registro encontrado : ");
 			System.out.println(cliente.getCodigo() + " - " + cliente.getPessoa().getNome());
-			
-			}
-		}
-		// -------------- EXCLUIR ----------------------
-		@Ignore
-		@Test
-		public void excluir() {
-			long codigo = 1L;
-			ClienteDAO clienteDAO = new ClienteDAO();
-			Cliente cliente = clienteDAO.buscar(codigo);
-			
-			
-			if(cliente == null) {
-				System.out.println("Nao foi encontrado nenhum registro do fabricante");
-			}else {
-				clienteDAO.excluir(cliente);
-			System.out.println("Registro de cliente removido com sucesso: ");
-			System.out.println("O Cliente" + cliente.getCodigo() + " - " + cliente.getPessoa().getNome() + " Foi removido ");
-			}
-			
-		}
-		// -------------- EDITAR ----------------------
-		@Test
-		//@Ignore
-		public void editar() {
-			long codigo = 1L;
-			ClienteDAO clienteDAO = new ClienteDAO();
-			Cliente cliente = clienteDAO.buscar(codigo);
-			
-			if(cliente == null) {
-				System.out.println("Nao foi encontrado nenhum registro desse fabricante");
-			}else {
-				System.out.println("Registro editado - antes :");
-				System.out.println(cliente.getCodigo() + " - " + cliente.getPessoa().getNome());
-			
-				cliente.getPessoa().setNome("Luis");
-				clienteDAO.editar(cliente);
-				
-				System.out.println("Registro editado - depois :");
-				System.out.println(cliente.getCodigo() + " - " + cliente.getPessoa().getNome());
-			}
-			
+
 		}
 	}
 
+	// -------------- EXCLUIR ----------------------
+	@Ignore
+	@Test
+	public void excluir() {
+		long codigo = 1L;
+		ClienteDAO clienteDAO = new ClienteDAO();
+		Cliente cliente = clienteDAO.buscar(codigo);
 
+		if (cliente == null) {
+			System.out.println("Nao foi encontrado nenhum registro do fabricante");
+		} else {
+			clienteDAO.excluir(cliente);
+			System.out.println("Registro de cliente removido com sucesso: ");
+			System.out.println(
+					"O Cliente" + cliente.getCodigo() + " - " + cliente.getPessoa().getNome() + " Foi removido ");
+		}
 
+	}
 
+	// -------------- EDITAR ----------------------
+	@Test
+	@Ignore
+	public void editar() {
+		long codigo = 1L;
+		ClienteDAO clienteDAO = new ClienteDAO();
+		Cliente cliente = clienteDAO.buscar(codigo);
 
+		if (cliente == null) {
+			System.out.println("Nao foi encontrado nenhum registro desse fabricante");
+		} else {
+			System.out.println("Registro editado - antes :");
+			System.out.println(cliente.getCodigo() + " - " + cliente.getPessoa().getNome());
+
+			cliente.getPessoa().setNome("Luis");
+			clienteDAO.editar(cliente);
+
+			System.out.println("Registro editado - depois :");
+			System.out.println(cliente.getCodigo() + " - " + cliente.getPessoa().getNome());
+		}
+
+	}
+}
--- Drogaria/src/test/java/br/com/luan/drogaria/dao/UsuarioDAOTest.java
+++ Drogaria/src/test/java/br/com/luan/drogaria/dao/UsuarioDAOTest.java
@@ -1,5 +1,7 @@
 package br.com.luan.drogaria.dao;
 
+import java.util.List;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -12,21 +14,105 @@ public class UsuarioDAOTest {
 	@Test
 	public void salvar() {
 		PessoaDAO pessoaDAO = new PessoaDAO();
-		Pessoa pessoa  = pessoaDAO.buscar(3L);
-		
+		Pessoa pessoa = pessoaDAO.buscar(3L);
+
 		System.out.println("Pessoa encontrada :");
 		System.out.println("Nome :" + pessoa.getNome());
 		System.out.println("CPF :" + pessoa.getCpf());
-		
+
 		Usuario usuario = new Usuario();
 		usuario.setAtivo(true);
 		usuario.setPessoa(pessoa);
 		usuario.setSenha("q1w2e3r4");
 		usuario.setTipo('A');
+
 		UsuarioDAO usuarioDAO = new UsuarioDAO();
 		usuarioDAO.salvar(usuario);
-		
+
 		System.out.println("O Usuario " + usuario.getPessoa().getNome() + " Foi adicionado ao Banco");
 	}
 
+	// -------------- LISTAR ----------------------
+	@Ignore
+	@Test
+	public void listar() {
+
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		List<Usuario> resultado = usuarioDAO.listar();
+
+		for (Usuario usuario : resultado) {
+			System.out.println("Código do Fabricante : " + usuario.getCodigo());
+			System.out.println("Nome do Usuario : " + usuario.getPessoa().getNome());
+			System.out.println("Senha : " + usuario.getSenha());
+			System.out.println("Tipo : " + usuario.getTipo());
+
+			if (usuario.getAtivo() == true) {
+				System.out.println("Ativo : ON");
+			} else {
+				System.out.println("Ativo : OFF");
+			}
+
+			System.out.println();
+		}
+	}
+
+	// -------------- BUSCAR ----------------------
+	@Test
+	@Ignore
+	public void buscar() {
+
+		long codigo = 1L;
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = usuarioDAO.buscar(codigo);
+
+		if (usuario == null) {
+			System.out.println("Nenhum registro encontrado");
+		} else {
+			System.out.println("Registro encontrado : ");
+			System.out.println(usuario.getCodigo() + " - " + usuario.getPessoa().getNome());
+
+		}
+	}
+
+	// -------------- EXCLUIR ----------------------
+	@Ignore
+	@Test
+	public void excluir() {
+		long codigo = 1L;
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = usuarioDAO.buscar(codigo);
+
+		if (usuario == null) {
+			System.out.println("Nao foi encontrado nenhum registro do fabricante");
+		} else {
+			usuarioDAO.excluir(usuario);
+			System.out.println("Registro de cliente removido com sucesso: ");
+			System.out.println(
+					"O Cliente" + usuario.getCodigo() + " - " + usuario.getPessoa().getNome() + " Foi removido ");
+		}
+
+	}
+
+	// -------------- EDITAR ----------------------
+	@Test
+	@Ignore
+	public void editar() {
+		long codigo = 1L;
+		UsuarioDAO usuarioDAO = new UsuarioDAO();
+		Usuario usuario = usuarioDAO.buscar(codigo);
+
+		if (usuario == null) {
+			System.out.println("Nao foi encontrado nenhum registro desse fabricante");
+		} else {
+			System.out.println("Registro editado - antes :");
+			System.out.println(usuario.getCodigo() + " - " + usuario.getPessoa().getNome());
+
+			usuario.getPessoa().setNome("Luis");
+			usuarioDAO.editar(usuario);
+
+			System.out.println("Registro editado - depois :");
+			System.out.println(usuario.getCodigo() + " - " + usuario.getPessoa().getNome());
+		}
+
+	}
 }
