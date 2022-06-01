378da0881ae3fc94f39f7263937cbfbdae26a456
TonhaodoArrroz <Luan.gustavo.melo@gmail.com>
2022-06-01 08:42:55 -0300

    Tentando colocar um banner rotativo na pagina index

---
+++ Drogaria/src/main/java/br/com/luan/drogaria/bean/Indexbean.java
@@ -0,0 +1,24 @@
+package br.com.luan.drogaria.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+ 
+@ManagedBean
+public class Indexbean {
+     
+    private List<String> images;
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<String>();
+        for (int i = 1; i <= 12; i++) {
+            images.add("banner" + i + ".jpg");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
