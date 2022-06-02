5056ac3120d65f28b7628304b5451a235f3f75d3
TonhaodoArrroz <Luan.gustavo.melo@gmail.com>
2022-06-02 14:51:26 -0300

    finalizado a parte de index e atualizando outros compenentes

--- Drogaria/src/main/java/br/com/luan/drogaria/bean/Indexbean.java
+++ Drogaria/src/main/java/br/com/luan/drogaria/bean/Indexbean.java
@@ -13,7 +13,7 @@ public class Indexbean {
     @PostConstruct
     public void init() {
         images = new ArrayList<String>();
-        for (int i = 1; i <= 12; i++) {
+        for (int i = 1; i <= 2; i++) {
             images.add("banner" + i + ".jpg");
         }
     }
